af86a6d58e2fb16486553b96886baa5865ba40b7
Tarik <49348194+tarizer@users.noreply.github.com>
2024-01-06 10:19:23 +0100

    Part 5: 1.Age check

---
+++ exercices/src/AgeCheck.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function AgeCheck() {
+  const onSubmitHandle = (e) => {
+    e.preventDefault();
+
+    const name = e.target.elements.name.value;
+    const age = e.target.elements.age.value;
+
+    if (age >= 18) alert("Hello " + name);
+    else alert("You are too young");
+  };
+  return (
+    <>
+      <form onSubmit={onSubmitHandle}>
+        <input type="text" placeholder="Name" name="name" />
+        <input type="number" placeholder="Age" name="age" />
+        <button type="submit">Check age</button>
+      </form>
+    </>
+  );
+}
+
+export default AgeCheck;
